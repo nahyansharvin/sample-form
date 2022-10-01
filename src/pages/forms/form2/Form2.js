427850ce93db917c2d902cc2ed7d495105b29f7d
Nahyan Sharvin <nskonnalath@gmail.com>
2022-10-02 00:01:42 +0530

    fix: code cleanup

--- src/pages/forms/form2/Form2.js
+++ src/pages/forms/form2/Form2.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import '../../Forms.css'
 import { useNavigate } from 'react-router-dom'
 
