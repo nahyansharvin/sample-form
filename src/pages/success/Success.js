c97074f5472050003db95516b35c34ecb175cafc
Nahyan Sharvin <nskonnalath@gmail.com>
2022-10-01 22:19:20 +0530

    add success page

---
+++ src/pages/success/Success.js
@@ -0,0 +1,15 @@
+import React,{useContext} from 'react'
+import './Success.css'
+
+import UserContext from '../../UserContext'
+
+function Success() {
+    const {user} = useContext(UserContext);
+  return (
+    <div className="success">
+        <h1>Thank You For Submitting The Form .</h1>
+    </div>
+  )
+}
+
+export default Success
