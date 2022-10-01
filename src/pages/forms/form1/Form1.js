04e8db5a5186a3a5a4a516b3147c8635e85ba66f
Nahyan Sharvin <nskonnalath@gmail.com>
2022-10-01 21:02:04 +0530

    form 1 done

--- src/pages/forms/form1/Form1.js
+++ src/pages/forms/form1/Form1.js
@@ -1,21 +1,39 @@
-import React,{ useState } from 'react'
-import '../Forms.css'
+import React,{ useState, useContext } from 'react'
+import '../../Forms.css'
 
 //Matrial UI
-import { Grid } from '@mui/material'
+import { Grid, Button } from '@mui/material'
 
 //Custom Components
 import TextInput from '../../../components/textInput/TextInput'
+import UserContext from '../../../UserContext'
 
 function Form1() {
+    const {user, setUser} = useContext(UserContext);
+
     //States
     const [name, setName] = useState()
     const [phoneNo, setPhoneNo] = useState()
     const [email, setEmail] = useState()
 
+    //Set error in empty fields
+    const setError = () => {
+        if(!name) setName("");
+        if(!phoneNo) setPhoneNo("");
+        if(!email) setEmail("");
+    }
+
+    //handle next button
+    const handleNextButton = () => {
+
+    }
+
+    
+
+
 
     return (
-        <div className="form-wrapper section-margin">
+        <div className="form-wrapper">
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <TextInput label="Name" value={name} setValue={setName} />                
@@ -25,6 +43,9 @@ function Form1() {
                 </Grid>
                 <Grid item xs={12}>
                     <TextInput label="Email" value={email} setValue={setEmail} />                
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" fullWidth>Next</Button>
                 </Grid>
                 
             </Grid>
